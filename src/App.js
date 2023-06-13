abc83db21f096cfcfa8d40d1f5510f8f590ce114
mamun-prodhan <mamunpr9@gmail.com>
2023-06-13 11:11:10 +0600

    simple concept

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const nayoks = ['sakib', 'kuber','bapparaz', 'salman shah', 'mannna', 'rubel'];
+  const singers = [
+    {name: 'dr.Razzak', job: 'singer' },
+    {name: 'Eva Rahman', job: 'housewife' },
+    {name: 'Siddik', job: 'actor' },
+    {name: 'hero alom', job: 'allrounder' },
+    {name: 'kelvin', job: 'worker' },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter</h1>
+      <Counter></Counter>
     </div>
   );
 }
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+  return(
+    <div>
+        <h2>{count}</h2>
+        <button onClick={increaseCount}>Increase</button> 
+        <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+
+
 
 export default App;
